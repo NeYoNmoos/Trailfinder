00afbf6f5d3da96331266cdf5230dfb03caf5e60
neyon <neyon@gmx.at>
2023-11-03 09:13:47 +0100

    generated entity classes with hibernate

---
+++ src/main/java/at/fhv/data/CoordinateEntity.java
@@ -0,0 +1,92 @@
+package at.fhv.data;
+
+import jakarta.persistence.*;
+
+import java.math.BigInteger;
+
+@Entity
+@Table(name = "coordinate", schema = "trailfinder_dev", catalog = "Trailfinder")
+public class CoordinateEntity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "coordinate_id")
+    private String coordinateId;
+    @Basic
+    @Column(name = "route_id")
+    private String routeId;
+    @Basic
+    @Column(name = "sequence")
+    private BigInteger sequence;
+    @Basic
+    @Column(name = "longitude")
+    private BigInteger longitude;
+    @Basic
+    @Column(name = "latitude")
+    private BigInteger latitude;
+
+    public String getCoordinateId() {
+        return coordinateId;
+    }
+
+    public void setCoordinateId(String coordinateId) {
+        this.coordinateId = coordinateId;
+    }
+
+    public String getRouteId() {
+        return routeId;
+    }
+
+    public void setRouteId(String routeId) {
+        this.routeId = routeId;
+    }
+
+    public BigInteger getSequence() {
+        return sequence;
+    }
+
+    public void setSequence(BigInteger sequence) {
+        this.sequence = sequence;
+    }
+
+    public BigInteger getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(BigInteger longitude) {
+        this.longitude = longitude;
+    }
+
+    public BigInteger getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(BigInteger latitude) {
+        this.latitude = latitude;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CoordinateEntity that = (CoordinateEntity) o;
+
+        if (coordinateId != null ? !coordinateId.equals(that.coordinateId) : that.coordinateId != null) return false;
+        if (routeId != null ? !routeId.equals(that.routeId) : that.routeId != null) return false;
+        if (sequence != null ? !sequence.equals(that.sequence) : that.sequence != null) return false;
+        if (longitude != null ? !longitude.equals(that.longitude) : that.longitude != null) return false;
+        if (latitude != null ? !latitude.equals(that.latitude) : that.latitude != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = coordinateId != null ? coordinateId.hashCode() : 0;
+        result = 31 * result + (routeId != null ? routeId.hashCode() : 0);
+        result = 31 * result + (sequence != null ? sequence.hashCode() : 0);
+        result = 31 * result + (longitude != null ? longitude.hashCode() : 0);
+        result = 31 * result + (latitude != null ? latitude.hashCode() : 0);
+        return result;
+    }
+}
