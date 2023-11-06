fc42d80d008407f6fdd36fc94bcba4408137541f
Mete Dastan <mete.dastan@live.at>
2023-11-06 14:34:52 +0100

    changed mock seedingscript

--- src/main/sql_scripts/mock_seeding/mock_route_data.sql
+++ src/main/sql_scripts/mock_seeding/mock_route_data.sql
@@ -1,6 +1,11 @@
 INSERT INTO trailfinder_dev.route(route_id, author, name, length, altitude, location, description, duration) VALUES
-     ('r001', 'u001', 'Trail 1', 5.50, 300, 'Location 1', 'A scenic trail', 2.00),
-     ('r002', 'u002', 'Trail 2', 8.25, 500, 'Location 2', 'A challenging trail', 2.60),
-     ('r003', 'u001', 'Trail 3', 3.75, 200, 'Location 3', 'A family-friendly trail', 1.30),
-     ('r004', 'u002', 'Trail 4', 6.00, 350, 'Location 4', 'A trail with great views', 2.30),
-     ('r005', 'u001', 'Trail 5', 10.00, 600, 'Location 5', 'A long-distance trail', 4.00);
+     ('r001', 'u001', 'Rappenlochschlucht', 4.70, 236, 'Dornbirn, Austria', 'Out & Back', 1.39),
+     ('r002', 'u001', 'Amannsbrücke', 4.20, 207, 'Dornbirn, Austria', 'Loop', 1.28),
+     ('r003', 'u001', 'Rund um den Staufen', 5.60, 227, 'Dornbirn, Austria', 'Loop', 1.47),
+     ('r004', 'u001', 'Aufs Gshol', 6.90, 483, 'Hohenems, Austria', 'Out & Back', 2.53),
+     ('r005', 'u001', 'Leiterweg', 6.40, 354, 'Hohenems, Austria', 'Loop', 2.21);
+     ('r006', 'u001', 'Waterfall Loop Alberschwende', 7.20, 251, 'Alberschwende, Austria', 'Loop', 2.09);
+     ('r007', 'u001', 'Uferweg und Schützingerweg', 2.10, 26, 'Lindau, Germany', 'Loop', 0.27);
+     ('r008', 'u001', 'Rorschach - Rheineck', 10.30, 125, 'Rorschach, Switzerland', 'Point to point', 2.15);
+     ('r009', 'u001', 'Pfänderwanderung ab Lochau', 10.90, 627, 'Lochau, Austria', 'Loop', 4.07);
+     ('r010', 'u001', 'Bödele und Hochälpelekopf', 9.20, 402, 'Schwarzenberg, Austria', 'Loop', 3.01);
