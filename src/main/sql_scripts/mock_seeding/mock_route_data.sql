87b3458b3453a21af4744b42efa440e086b936b6
Mete Dastan <metedastan@unknown-60-3e-5f-82-96-10-MBP-von-Mete.wlan.uclv.net>
2023-11-30 12:14:47 +0100

    changed mock seedingscript

--- src/main/sql_scripts/mock_seeding/mock_route_data.sql
+++ src/main/sql_scripts/mock_seeding/mock_route_data.sql
@@ -1,5 +1,15 @@
 
+DELETE FROM trailfinder_dev.coordinate WHERE route_id BETWEEN 1 AND 20;
+DELETE FROM trailfinder_dev.route WHERE route_id BETWEEN 1 AND 20;
+DELETE FROM trailfinder_dev.attribute WHERE attribute_id BETWEEN 1 AND 20;
 
+
+INSERT INTO trailfinder_dev.attribute(attribute_id, strength, condition, scenery, experience) VALUES
+     (1, 3, 3, 4, 3),
+     (2, 3, 3, 4, 2),
+     (3, 3, 3, 3, 2),
+     (4, 4, 3, 4, 3),
+     (5, 3, 2, 4, 4);
 
 INSERT INTO trailfinder_dev.route(route_id, author, name, length, altitude, location, description, duration, attribute_id, months, active) VALUES
      (1, NULL, 'Rappenlochschlucht', 4.70, 236, 'Dornbirn, Austria', 'Entdecke diesen 4,7-Kilometer langen Hin- und Rückweg in der Nähe von Dornbirn, Vorarlberg. Die Route wird grundsätzlich als moderat eingestuft und kann zumeist in 1 Std 39 Min bewältigt werden. Da sich der Weg bestens zum Wandern eignet, bist du selten allein unterwegs und wirst während deines Ausflugs auch auf andere Abenteurer treffen. Hunde sind erlaubt, müssen aber angeleint sein.', 1.39, 1, 1020, true),
@@ -8,10 +18,7 @@ INSERT INTO trailfinder_dev.route(route_id, author, name, length, altitude, loca
      (4, NULL, 'Aufs Gshol', 6.90, 483, 'Hohenems, Austria', 'Erlebe diesen 6,9-Kilometer langen Hin- und Rückweg in der Nähe von Hohenems, Vorarlberg. Die Route wird grundsätzlich als schwierig eingestuft und kann zumeist in 2 Std 53 Min bewältigt werden. Da sich der Weg bestens zum Wandern eignet, bist du selten allein unterwegs und wirst während deines Ausflugs auch auf andere Abenteurer treffen.', 2.53, 4, 1020, true),
      (5, NULL, 'Leiterweg', 6.40, 354, 'Hohenems, Austria', 'Genieße diesen 6,4-Kilometer langen Rundweg in der Nähe von Hohenems, Vorarlberg. Die Route wird grundsätzlich als moderat eingestuft und kann zumeist in 2 Std 21 Min bewältigt werden. Obwohl sich der Weg bestens zum Wandern eignet und oftmals gut besucht ist, kannst du hier zur richtigen Tageszeit ein bisschen Ruhe genießen.', 2.21, 5, 1020, true);
 
-INSERT INTO trailfinder_dev.route(route_id, author, name, length, altitude, location, description, duration, attribute_id, months, active, created_at) VALUES
-     (32, 1, 'Rappenlochschlucht', 4.70, 236, 'Dornbirn, Austria', 'Entdecke diesen 4,7-Kilometer langen Hin- und Rückweg in der Nähe von Dornbirn, Vorarlberg. Die Route wird grundsätzlich als moderat eingestuft und kann zumeist in 1 Std 39 Min bewältigt werden. Da sich der Weg bestens zum Wandern eignet, bist du selten allein unterwegs und wirst während deines Ausflugs auch auf andere Abenteurer treffen. Hunde sind erlaubt, müssen aber angeleint sein.', 1.39, 1, 1020, true, NULL);
 
-SELECT * FROM trailfinder_dev.route;
 
 INSERT INTO trailfinder_dev.coordinate(coordinate_id, route_id, sequence, longitude, latitude) VALUES
     (1, 1, 0, 9.775992718176056, 47.391678657852516),
@@ -30,24 +37,9 @@ INSERT INTO trailfinder_dev.coordinate(coordinate_id, route_id, sequence, longit
     (14, 5, 2, 9.691213642617338, 47.36441490065213);
 
 
-INSERT INTO trailfinder_dev.attribute(attribute_id, strength, condition, scenery, experience) VALUES
-     (1, 3, 3, 4, 3),
-     (2, 3, 3, 4, 2),
-     (3, 3, 3, 3, 2),
-     (4, 4, 3, 4, 3),
-     (5, 3, 2, 4, 4);
-
-DELETE FROM trailfinder_dev.route WHERE route_id BETWEEN 1 AND 20;
-DELETE FROM trailfinder_dev.coordinate WHERE route_id BETWEEN 1 AND 20;
-DELETE FROM trailfinder_dev.attribute WHERE attribute_id BETWEEN 1 AND 20;
-
-
-
-
-
 
 /*
- ergänzungen:
+ Ergänzungen:
   (6, 'u001', 'Waterfall Loop Alberschwende', 7.20, 251, 'Alberschwende, Austria', 'Loop', 2.09, 6, 1020, true),
   (7, 'u001', 'Uferweg und Schützingerweg', 2.10, 26, 'Lindau, Germany', 'Loop', 0.27, 7, 2044, true),
   (8, 'u001', 'Rorschach - Rheineck', 10.30, 125, 'Rorschach, Switzerland', 'Point to point', 2.15, 8, 1022, true),
